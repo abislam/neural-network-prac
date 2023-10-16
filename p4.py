8940083428de304bd5ff3ba51ab82350821d24ad
Abed <abislam@iu.edu>
2023-10-16 19:37:33 -0400

    Initial commit practicing neural network setup in python

---
+++ p4.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+#3 neurons 4 inputs
+
+'''
+Doing dot product for the weights and inputs
+'''
+
+inputs = [1, 2, 3, 2.5]
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2.0, 3.0, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
