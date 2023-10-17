1662054ae475425c280d625ab51c43f8f16b35a0
Abed <abislam@iu.edu>
2023-10-16 22:36:25 -0400

    added second layer to network

--- p4.py
+++ p4.py
@@ -1,27 +1,35 @@
 import numpy as np
 
 
-#3 neurons 4 inputs
-
 '''
 Batches, Layers, and Objects
-
-
 '''
 #3 batches of 4 inputs
+
+#initial inputs
+#3 neurons 4 inputs
 inputs = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
 
-
-weights = [[0.2, 0.8, -0.5, 1.0],
+#1st layer
+weights1 = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2.0, 3.0, 0.5]
+biases1 = [2.0, 3.0, 0.5]
+
+#2nd layer - only 3 weights because the outputs of the first layer went to 3 neurons in the second layer
+weights2 = [[0.1, -0.14, 0.5],
+           [-0.5, 0.12, -0.33],
+           [-0.44, 0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
 
 #need to transpose weights matrix, otherwise shape error
 #convert weights to numpy array
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+layer1_outputs = np.dot(inputs, np.array(weights1).T) + biases1
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2 #output of layer1 became input of layer2
+
+print(layer2_outputs)
