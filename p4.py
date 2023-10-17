7ae3d2f2a2e86013a4301caee2e42f0b7ae2e737
Abed <abislam@iu.edu>
2023-10-16 22:57:40 -0400

    updated to object oriented code. Check commit history for code without OOP

--- p4.py
+++ p4.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+np.random.seed(0)
+
 '''
 Batches, Layers, and Objects
 '''
@@ -8,28 +10,22 @@ Batches, Layers, and Objects
 
 #initial inputs
 #3 neurons 4 inputs
-inputs = [[1, 2, 3, 2.5],
+#Sample training data. Variable name X is a convention for training data in Neural Networks.
+X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons) #generate weights based on range of #inputs and #neurons
+        self.biases = np.zeros((1, n_neurons)) # 1 * n_neurons matrix of biases
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-#1st layer
-weights1 = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+layer1 = Layer_Dense(4,5) # we know from X that initial number of inputs is 4, number of neurons is up to you
+layer2 = Layer_Dense(5,2) # since layer1 had 5 neurons, layer 2 will have 5 inputs, again, number of neurons is up to you
 
-biases1 = [2.0, 3.0, 0.5]
-
-#2nd layer - only 3 weights because the outputs of the first layer went to 3 neurons in the second layer
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-#need to transpose weights matrix, otherwise shape error
-#convert weights to numpy array
-layer1_outputs = np.dot(inputs, np.array(weights1).T) + biases1
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2 #output of layer1 became input of layer2
-
-print(layer2_outputs)
+layer1.forward(X) #forward propogate layer1
+print(layer1.output)
+layer2.forward(layer1.output) #after layer1 forward propogation, forward propogate layer 2. In this case its the final output
+print(layer2.output)
