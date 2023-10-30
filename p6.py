7c31978857b7b0f0f76c845dc377543e322d3c83
Abed <abislam@iu.edu>
2023-10-29 20:33:18 -0400

    exponentiation and normalization for batches

--- p6.py
+++ p6.py
@@ -1,10 +1,10 @@
-import math
+
 import numpy as np
 
 #softmax activation
 
 #dummy values
-layer_outputs = [4.8, 1.21, 2.385]
+layer_outputs = [[4.8, 1.21, 2.385], [8.9, -1.81, 0.2], [1.41, 1.051, 0.026]]
 
 
 #applying exponentiation. handles negative data without losing meaning of said data
@@ -17,7 +17,12 @@ print('exponentiated values:')
 print(exp_values)
 
 #normalization of exp_values using numpy
-norm_values = exp_values / np.sum(exp_values)
-print('normalized exponentiated values:')
+
+#axis 0 takes sum of columns, not what we want
+#print(np.sum(layer_outputs, axis = 0))
+ 
+#axis 1 takes sum of rows, this is what we want, keepdims true ensures same dimensions as input
+#print(np.sum(layer_outputs, axis = 1, keepdims = True)) 
+
+norm_values = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
 print(norm_values)
-print('sum of normalized values:', np.sum(norm_values))
