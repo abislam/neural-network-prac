9dfee1ced61f170d07818c35aeec627fb931bb19
Abed <abislam@iu.edu>
2023-10-29 20:15:46 -0400

    exponentiation and normalization using numpy

--- p6.py
+++ p6.py
@@ -1,30 +1,23 @@
 import math
+import numpy as np
 
 #softmax activation
 
-
+#dummy values
 layer_outputs = [4.8, 1.21, 2.385]
 
 
 #applying exponentiation. handles negative data without losing meaning of said data
 #important for calculating probabilities.
 
-E = math.e #eulers nhumber
-
-exp_values = []
-
-for output in layer_outputs:
-        exp_values.append (E ** output) # ** means to the power
+#exponentiation using numpy
+# for loop, for each value in vector layer_outputs, exponentiate the value, store in exp_values
+exp_values = np.exp(layer_outputs)
 print('exponentiated values:')
 print(exp_values)
 
-#normalization of exp_values
-norm_base = sum(exp_values)
-norm_values = []
-
-for value in exp_values:
-        norm_values.append(value / norm_base)
-print('Normalized exponentiated values:')
+#normalization of exp_values using numpy
+norm_values = exp_values / np.sum(exp_values)
+print('normalized exponentiated values:')
 print(norm_values)
-
-print('Sum of normalized values:', sum(norm_values))
+print('sum of normalized values:', np.sum(norm_values))
