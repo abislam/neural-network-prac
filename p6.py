cfc5cecb812302e3c23b79a87ffbe38392e30462
Abed <abislam@iu.edu>
2023-10-29 20:34:43 -0400

    softmax in OOP

--- p6.py
+++ p6.py
@@ -1,28 +1,42 @@
 
-import numpy as np
-
-#softmax activation
-
-#dummy values
-layer_outputs = [[4.8, 1.21, 2.385], [8.9, -1.81, 0.2], [1.41, 1.051, 0.026]]
+import numpy as np 
+import nnfs
+from nnfs.datasets import spiral_data
 
 
-#applying exponentiation. handles negative data without losing meaning of said data
-#important for calculating probabilities.
+nnfs.init()
 
-#exponentiation using numpy
-# for loop, for each value in vector layer_outputs, exponentiate the value, store in exp_values
-exp_values = np.exp(layer_outputs)
-print('exponentiated values:')
-print(exp_values)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-#normalization of exp_values using numpy
 
-#axis 0 takes sum of columns, not what we want
-#print(np.sum(layer_outputs, axis = 0))
- 
-#axis 1 takes sum of rows, this is what we want, keepdims true ensures same dimensions as input
-#print(np.sum(layer_outputs, axis = 1, keepdims = True)) 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
 
-norm_values = exp_values / np.sum(exp_values, axis = 1, keepdims = True)
-print(norm_values)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2,3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
